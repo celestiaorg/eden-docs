30ac2bd40febeb2508ea605b2a7b42fbd1f19c94
jcstein <46639943+jcstein@users.noreply.github.com>
2025-08-28 15:58:00 -0600

    docs: add wagmi guide

--- app/_meta.js
+++ app/_meta.js
@@ -2,6 +2,6 @@ export default {
   networks: "Networks",
   tooling: "Tooling",
   "development-environments": "Development environments",
-  "front-end-libraries": "Frontend libraries",
+  "frontend-libraries": "Frontend libraries",
   guides: "Guides",
 };
