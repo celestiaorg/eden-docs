fb3866a1701e20dab285b0fd8303aeb813e5a55f
jcstein <46639943+jcstein@users.noreply.github.com>
2025-09-17 15:42:55 -0600

    fix: trailing comma dangle

--- app/tooling/account-abstraction/_meta.js
+++ app/tooling/account-abstraction/_meta.js
@@ -1,7 +1,6 @@
-
 export default {
   'getting-started': 'Getting started',
   operations: 'Operations, not transactions',
   'sponsored-transactions': 'Sponsored transactions',
-  'session-keys': 'Session keys',
+  'session-keys': 'Session keys'
 }
