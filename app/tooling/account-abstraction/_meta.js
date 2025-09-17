804ee2f636fb9c3c8d7b7e316bfd6d4e6a1657c5
Gabriel Barros <gabrielb@celestia.org>
2025-09-17 11:52:16 -0300

    feat: add account abstraction documentation and session keys overview

---
+++ app/tooling/account-abstraction/_meta.js
@@ -0,0 +1,7 @@
+
+export default {
+  'getting-started': 'Getting started',
+  operations: 'Operations, not transactions',
+  'sponsored-transactions': 'Sponsored transactions',
+  'session-keys': 'Session keys',
+}
