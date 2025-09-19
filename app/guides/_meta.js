b98066aa1cad4e5d9a8b695546e6858ca92a8037
jcstein <46639943+jcstein@users.noreply.github.com>
2025-09-18 23:59:25 -0600

    docs: add guides for repos

--- app/guides/_meta.js
+++ app/guides/_meta.js
@@ -1,5 +1,4 @@
 export default {
   web2toweb3: 'Onchain primer for web2 devs',
-  'build-a-frontend': 'Build a frontend',
-  'create-eden-app': 'create-eden-app'
+  'build-a-frontend': 'Build a frontend'
 }
