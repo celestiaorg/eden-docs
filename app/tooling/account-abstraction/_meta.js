4ffaa29198797dd12f0c5d1ff53a0df33c9b9b1b
Gabriel Barros <gabrielb@celestia.org>
2025-09-19 03:04:17 -0300

    [WIP] Docs: Account Abstraction section — getting started, operations, session keys, sponsored transactions  Summary (#70)
    * feat: add account abstraction documentation and session keys overview
    
    * feat: enhance session keys documentation and add sponsored transactions overview
    
    * fix: trailing comma dangle
    
    * fix: format w prettier
    
    * docs: add guides for repos
    
    * fix: format
    
    ---------
    
    Co-authored-by: Josh Stein <46639943+jcstein@users.noreply.github.com>

---
+++ app/tooling/account-abstraction/_meta.js
@@ -0,0 +1,9 @@
+export default {
+  overview: 'Overview',
+  quickstart: 'Quickstart with ZeroDev & Privy',
+  operations: 'Operations, not transactions',
+  'sponsored-transactions': 'Sponsored transactions',
+  'session-keys': 'Session keys',
+  example: 'Example with ZeroDev & Privy',
+  'react-wallet-panel': 'React wallet panel'
+}
