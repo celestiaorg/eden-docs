85bf32fd1ac6af02d842a306d47eed08aa4d8761
jcstein <46639943+jcstein@users.noreply.github.com>
2025-09-22 14:10:09 -0600

    docs: rearrange aa section

--- app/tooling/account-abstraction/_meta.js
+++ app/tooling/account-abstraction/_meta.js
@@ -1,9 +1,6 @@
 export default {
   overview: 'Overview',
   quickstart: 'Quickstart with ZeroDev & Privy',
-  operations: 'Operations, not transactions',
-  'sponsored-transactions': 'Sponsored transactions',
-  'session-keys': 'Session keys',
   example: 'Example with ZeroDev & Privy',
   'react-wallet-panel': 'React wallet panel'
 }
