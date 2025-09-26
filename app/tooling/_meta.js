d81b2cb387dbe88b7f09e509b3960da3b83d03c1
jcstein <46639943+jcstein@users.noreply.github.com>
2025-09-26 09:23:46 -0600

    docs: add existing testnet contractsg

--- app/tooling/_meta.js
+++ app/tooling/_meta.js
@@ -1,7 +1,6 @@
 export default {
   'account-abstraction': 'Account abstraction',
   bridging: 'Bridging',
-  'existing-contracts': 'Existing contracts',
   indexers: 'Indexers',
   oracles: 'Oracles',
   storage: 'Storage',
