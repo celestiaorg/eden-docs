ad5c827085b07ac59250c3431e2e0085457a35e0
Luke Cassady-Dorion <luke@SPStories.com>
2025-10-24 21:44:58 +0700

    Reworked menu structure (#97)
    * Reworked menu structure
    
    * Set up shared path aliases for components/lib and update MDX imports
    
    * fix: lint
    
    ---------
    
    Co-authored-by: Gabriel Barros <gabrielb@celestia.org>
    Co-authored-by: jcstein <z@zs-MacBook-Air.local>

--- app/_meta.js
+++ app/_meta.js
@@ -1,6 +1,8 @@
 export default {
   networks: 'Networks',
   'development-environments': 'Development environments',
+  vaults: 'Vaults',
+  tokens: 'Tokens',
   'frontend-libraries': 'Frontend libraries',
   tooling: 'Tooling',
   guides: 'Guides'
